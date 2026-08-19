e364e687e784166433b1eba4e7fb0e2f71b3d031
Laboratório FIAP <labsfiap@fiap.com.br>
2026-08-19 20:28:37 -0300

    feat: aula01 - implementa classes Passafeiro e desafio pessoal

---
+++ src/br/com/fiapride/model/Kindle.java
@@ -0,0 +1,10 @@
+package br.com.fiapride.model;
+
+public class Kindle {
+	
+	public String id;
+	public double versao;
+	public String cor;
+	public double capacidadeEmGB;
+
+}
---
+++ src/br/com/fiapride/model/Passageiro.java
@@ -0,0 +1,13 @@
+package br.com.fiapride.model;
+
+public class Passageiro {
+
+	// public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	public String nome;
+	public double saldo;
+		
+	}
+
+// }
